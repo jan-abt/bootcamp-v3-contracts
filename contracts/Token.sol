3971a8ac4108323df1f48e07ac85527c4cb6b511
jan-abt <jan.abt@gmail.com>
2025-06-13 17:46:28 +0700

    2. Smoke Test

---
+++ contracts/Token.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.28;
+
+contract Token {
+    // state variable: 
+    // permanently writes a value assigned to  "name", to the blockchain 
+    string public name = "My Token";
+
+
+}
