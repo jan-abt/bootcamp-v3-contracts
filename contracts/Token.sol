67d985e634d469ecb3af0a0da5893b3c68ebd128
jan-abt <jan.abt@gmail.com>
2025-06-14 20:16:56 +0700

    3. Testing Contracts

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,9 +2,19 @@
 pragma solidity 0.8.28;
 
 contract Token {
-    // state variable: 
-    // permanently writes a value assigned to  "name", to the blockchain 
-    string public name = "My Token";
+    // state variables : 
+    // permanently writes values to the blockchain 
+    string public name;
+    string public symbol;
+    uint8  public decimals = 18;
+    uint256 public totalSupply;
 
-
+    constructor(
+        string memory _name, 
+        string memory _symbol, 
+        uint256 _totalSupply){
+            name = _name;
+            symbol = _symbol;
+            totalSupply = _totalSupply  * (10 ** decimals); // in base units (for ETH, wei)
+    }
 }
