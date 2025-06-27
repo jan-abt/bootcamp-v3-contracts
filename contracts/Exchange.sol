d16c4a2564bb1bf3f4040ddea8fa66c451b8066c
jan-abt <jan.abt@gmail.com>
2025-06-27 21:02:09 +0700

    11. Make Orders

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -3,8 +3,8 @@ pragma solidity 0.8.28;
 
 import {Token} from "./Token.sol";
 
-// TODO: On the Exchange 
-// 1. Depoist Tokens 
+// TODO: On the Exchange
+// 1. Depoist Tokens
 // 2. Withdraw Tokens
 // 3. Check Balances
 // 4. Make Orders
@@ -15,30 +15,58 @@ import {Token} from "./Token.sol";
 
 contract Exchange {
     // state variables
-    address public  feeAccount;
+    address public feeAccount;
     uint256 public feePercent;
+    uint256 public orderCount;
+
+    // Mappings
+    mapping(uint256 => Order) public orders;
 
     // total tokens belonging to a user
-    // token address     
-    mapping( address => 
-        // user address
-        mapping(address => uint256)) 
-            private userTotalTokenBalance;
+    // (token address mapping -> user address mapping)
+    mapping(address => mapping(address => uint256))
+        private userTotalTokenBalance;
+
+    // total tokens on an active order
+    // (token address mapping -> user address mapping)
+    mapping(address => mapping(address => uint256))
+        private userActiveTokenBalance;
 
     event TokensDeposited(
-        address token, 
-        address user, 
-        uint256 amount, 
-        uint256 balance
-    );
-    event TokensWithdrawn(
-        address token, 
-        address user, 
-        uint256 amount, 
+        address token,
+        address user,
+        uint256 amount,
         uint256 balance
     );
 
-    constructor( address _feeAccount, uint256 _feePercent ){
+    event TokensWithdrawn(
+        address token,
+        address user,
+        uint256 amount,
+        uint256 balance
+    );
+
+    event OrderCreated(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,
+        uint256 timestamp
+    );
+
+    struct Order {
+        uint256 id; // unique identifier for an order
+        address user; // user who made the order
+        address tokenGet; // address of the token they receive
+        uint256 amountGet; // amount they receive
+        address tokenGive; // address of the token they give
+        uint256 amountGive; // amount they give
+        uint256 timestamp; // when the order was created
+    }
+
+    constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
         feePercent = _feePercent;
     }
@@ -46,11 +74,10 @@ contract Exchange {
     // --------------
     // DEPOSIT & WITHDRWA TOKENS
 
-    function depositToken(address _token, uint256 _amount) public{
-        
+    function depositToken(address _token, uint256 _amount) public {
         // take token out of user's wallet
         require(
-            Token (_token).transferFrom(msg.sender, address(this), _amount),
+            Token(_token).transferFrom(msg.sender, address(this), _amount),
             "Exchange Token transfer failed"
         );
 
@@ -59,37 +86,90 @@ contract Exchange {
 
         //  emit event
         emit TokensDeposited(
-            _token, 
-            msg.sender, 
-            _amount, 
+            _token,
+            msg.sender,
+            _amount,
             userTotalTokenBalance[_token][msg.sender]
         );
     }
 
     function totalBalanceOf(
-        address _token, 
-        address _user) public view returns (uint256){
-            return userTotalTokenBalance[_token][_user];
-
+        address _token,
+        address _user
+    ) public view returns (uint256) {
+        return userTotalTokenBalance[_token][_user];
     }
 
-    function withdrawToken(address _token, uint256 _amount) public {
+    function activeBalanceOf(
+        address _token,
+        address _user
+    ) public view returns (uint256) {
+        return userActiveTokenBalance[_token][_user];
+    }
 
-        require(totalBalanceOf(_token, msg.sender) >= _amount, "Exchange: Insufficient balance");
+
+    function withdrawToken(address _token, uint256 _amount) public {
+        require(
+            totalBalanceOf(_token, msg.sender) - activeBalanceOf(_token, msg.sender) >= _amount,
+            "Exchange: Insufficient balance"
+        );
         // update user balance
         userTotalTokenBalance[_token][msg.sender] -= _amount;
 
         // emit the event
         emit TokensWithdrawn(
             _token,
-            msg.sender, 
-            _amount, 
+            msg.sender,
+            _amount,
             userTotalTokenBalance[_token][msg.sender]
         );
 
         // transfer tokens back to user
-        require(Token(_token).transfer(msg.sender, _amount), "Exchange: Token transfer failed") ;
-
+        require(
+            Token(_token).transfer(msg.sender, _amount),
+            "Exchange: Token transfer failed"
+        );
     }
 
+    // ---------------------
+    // MAKE & CANCEL ORDERS
+
+    function makeOrder(
+        address _tokenGet,
+        uint256 _amountGet,
+        address _tokenGive,
+        uint256 _amountGive
+    ) public {
+        require(
+            totalBalanceOf(_tokenGive, msg.sender) >=  activeBalanceOf(_tokenGive, msg.sender) + _amountGive,
+            "Exchange: Insufficient balance"
+        );
+
+        orderCount++;
+
+        // instantiate a new order
+        orders[orderCount] = Order(
+            orderCount,
+            msg.sender,
+            _tokenGet,
+            _amountGet,
+            _tokenGive,
+            _amountGive,
+            block.timestamp
+        );
+
+        // update user's active balance
+        userActiveTokenBalance[_tokenGive][msg.sender] += _amountGive;
+
+        // emit the event
+        emit OrderCreated(
+            orderCount,
+            msg.sender,
+            _tokenGet,
+            _amountGet,
+            _tokenGive,
+            _amountGive,
+            block.timestamp
+        );
+    }
 }
--- test/Exchange.js
+++ test/Exchange.js
@@ -1,7 +1,7 @@
 const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers")
 const { expect } = require("chai")
 const { ethers } = require("hardhat")
-const { tokens, deployExchangeFixture, depositExchangeFixture } = require("./helpers/ExchangeFixtures")
+const { tokens, deployExchangeFixture, depositExchangeFixture, orderExchangeFixture} = require("./helpers/ExchangeFixtures")
 
 describe("Exchange", () => {
     describe("Deployment",()=>{
@@ -93,6 +93,58 @@ describe("Exchange", () => {
                 await expect(exchange.connect(accounts.user1)
                 .withdrawToken(
                     await token0. getAddress(), AMOUNT)
+                ).to.be.revertedWith(ERROR)
+            
+            })
+             
+        })
+    })
+
+
+
+    describe("Making Orders", ()=>{
+
+      describe("Success",()=>{
+        
+            it("it tracks the newly created order", async() =>{
+                    const { exchange } = await loadFixture(orderExchangeFixture)   
+                    expect(await exchange.orderCount()).to.equal(1)              
+                
+            })
+            it("emits a OrderCreated event", async() =>{
+               const {tokens:{ token0, token1 }, exchange, accounts, transaction} = await loadFixture(orderExchangeFixture)                
+               
+               const ORDER_ID = 1
+               const AMOUNT = tokens(1)
+               await ethers.provider.getBlock()
+               const { timestamp } = await ethers.provider.getBlock()
+               await expect(transaction).to.emit(exchange, "OrderCreated")
+                .withArgs(
+                        ORDER_ID,
+                        accounts.user1.address,
+                        await token1.getAddress(),
+                        AMOUNT,
+                        await token0.getAddress(),
+                        AMOUNT,
+                        timestamp
+                    )
+            })
+                    
+        })
+        describe("Failiure",()=>{
+            
+            it("Rejects with no balance", async() =>{
+                const {tokens:{ token0, token1 }, exchange, accounts } = await loadFixture(deployExchangeFixture)                
+                const ERROR = "Exchange: Insufficient balance"
+
+                await expect(
+                    exchange.connect(accounts.user1)
+                    .makeOrder(
+                        await token1.getAddress(),
+                        tokens(1),
+                        await token0.getAddress(),
+                        tokens(1),
+                    )
                 ).to.be.revertedWith(ERROR)
             
             })
