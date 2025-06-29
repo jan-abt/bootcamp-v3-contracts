c5fa686723d6efaaf0280e713d3a3c5be124251c
jan-abt <jan.abt@gmail.com>
2025-06-29 18:15:27 +0700

    15 Flash Loans Pt.2

--- contracts/FlashLoanUser.sol
+++ contracts/FlashLoanUser.sol
@@ -3,10 +3,13 @@ pragma solidity 0.8.28;
 
 import {Token} from "./Token.sol";
 import {Exchange} from "./Exchange.sol";
+import {IFlashLoanReceiver} from "./FlashLoanProvider.sol";
 
-contract FlashLoanUser {
+contract FlashLoanUser is IFlashLoanReceiver{
 
     address exchange;
+    event FlashLoanReceived(address token, uint256 amount);
+
     constructor(address _exchange){
         exchange = _exchange;
     }
@@ -14,6 +17,26 @@ contract FlashLoanUser {
     function getFlashLoan(address _token, uint256 _amount) external{
         // Call the exchange for the flash loan
         Exchange(exchange).flashLoan(_token, _amount, "");
+
+    }
+
+    function receiveFlashLoan(
+        address _token,
+        uint256 _amount, 
+        bytes memory /* _data*/
+    ) external {
+
+        require(msg.sender == exchange, "FlashLoanUser: Not Exchange contract");
+
+        // Do something with our loan
+        emit FlashLoanReceived(_token, Token(_token).balanceOf(address(this)));
+
+        // Pay back the loan       
+        require(
+            // typecast: interprets the token address as a token contract instance
+            Token(_token).transfer(exchange, _amount),
+            "FlashLoanUser: Transfer failed"
+        );
 
     }
 
--- test/FlashLoanProvider.js
+++ test/FlashLoanProvider.js
@@ -22,23 +22,55 @@ describe("FlashLoanProvider", () => {
 
     describe("Calling flashLoan from FlashLoanUser",()=>{
 
-        const AMOUNT = tokens(100)
         
-        it("Emits FlashLoan event", async() =>{
-            const { tokens: { token0 }, exchange, accounts, flashLoanUser} = await loadFixture(flashLoanFixture)
-            const flashLoanTx = await flashLoanUser
-                                        .connect(accounts.user1)
-                                        .getFlashLoan(
-                                                await token0.getAddress(), 
-                                                AMOUNT
-                                        )
-            await expect(flashLoanTx).to.emit(exchange, "FlashLoan")
 
-             
+        describe("Success",()=>{ 
+
+            const AMOUNT = tokens(100)
+
+            it("Emits FlashLoanReceived event", async() =>{
+                const { tokens: { token0 }, accounts, flashLoanUser} = await loadFixture(flashLoanFixture)
+                const flashLoanTx = await flashLoanUser
+                                            .connect(accounts.user1)
+                                            .getFlashLoan(
+                                                    await token0.getAddress(), 
+                                                    AMOUNT
+                                            )
+                await expect(flashLoanTx).to.emit(flashLoanUser, "FlashLoanReceived")
+                
+            })
+            
+            it("Emits FlashLoan event", async() =>{
+                const { tokens: { token0 }, exchange, accounts, flashLoanUser} = await loadFixture(flashLoanFixture)
+                const flashLoanTx = await flashLoanUser
+                                            .connect(accounts.user1)
+                                            .getFlashLoan(
+                                                    await token0.getAddress(), 
+                                                    AMOUNT
+                                            )
+                await expect(flashLoanTx).to.emit(exchange, "FlashLoan")
+                
+            })
         })
+
+        describe("Failiure",()=>{
+                         
+            it("Rejects on insufficient funds", async() =>{
+
+                const { tokens: { token1 }, exchange, accounts} = await loadFixture(deployExchangeFixture)
+                await expect( exchange.connect(accounts.user1).flashLoan(
+                            await token1.getAddress(), 
+                            tokens(100),
+                            "0x"
+                )).to.be.revertedWith("FlashLoanProvider: Insufficient funds to loan")            
+            })             
+        })       
+    
     })
 
-    describe("Test Template", ()=>{
+})
+
+describe("Test Template", ()=>{
 
         describe("Success",()=>{
         
@@ -56,6 +88,4 @@ describe("FlashLoanProvider", () => {
             })
              
         })
-    })
-
 })   
