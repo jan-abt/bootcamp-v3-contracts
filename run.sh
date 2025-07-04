7f7f4d008e7310793a4c743520df258bdb081378
jan-abt <jan.abt@gmail.com>
2025-07-04 14:06:11 +0700

    start-up script refactorings

--- run.sh
+++ run.sh
@@ -1,34 +1,36 @@
 #!/bin/bash
 
+########################################
+#  * Start Hardhat (in-memory blockchain) 
+#  * Deploy Contracts
+########################################
+
+
 # Exit script if any command fails
 set -e
 
 # set -x  # print each command before executing (for debugging)
 
-# kill any process using port 8545
-kill_port_8545() {
-  PID=$(lsof -ti tcp:8545)
-  if [ -n "$PID" ]; then
-    echo "🔓  Port 8545 is in use by PID $PID. Killing it..."
-    echo "    (or discover PID manually via lsof -wni tcp:8545)"
-    if kill -9 "$PID"; then
-      echo "💀 Process $PID has been killed."
-    else
-      echo "⚠️  Failed to kill PID $PID. Continuing anyway..."
-    fi
-  else
-    echo "Port 8545 is 🆓."
-  fi
-}
+printf "\nStarting Hardhat node..."
 
 # Check and/or kill process using port 8545
-kill_port_8545
+# get PID using port 8545 (don't fail if empty)
+PID=$(lsof -ti tcp:8545 || true)
+if [ -n "$PID" ]; then
+  printf "\n🔓  Port 8545 is in use by PID $PID."
+  printf "\n    (Find PID manually via lsof -wni tcp:8545)"
+  printf "\n Killing it ..."
+  if kill -9 "$PID"; then
+    printf "\n 💀 Process $PID has been killed.\n"
+  else
+    printf "\n⚠️  Failed to kill PID $PID. Continuing anyway..."
+  fi
+fi
 
-printf "\nStarting Hardhat node..."
 npx hardhat node > node.log 2>&1 &
-echo "Started Hardhat node with PID $!"
+printf "\nStarted Hardhat node with PID $!\n"
 
-printf "\n⏳ Wait for 5 seconds...\n\n"
+printf "\n⏳ Waiting for 5 seconds...\n\n"
 sleep 5
 
 npx hardhat ignition deploy ignition/modules/Token.js --network localhost
@@ -37,10 +39,5 @@ npx hardhat ignition deploy ignition/modules/Exchange.js --network localhost
 
 npx hardhat ignition deploy ignition/modules/FlashLoanUser.js --network localhost
 
-printf "\n⏳ Wait for 5 seconds..."
-sleep 5
+printf "\n👍 Run script completed successfully.\n"
 
-printf "\n  Running seed script...\n\n"
-npx hardhat run scripts/seed.js --network localhost
-
-printf "\n👍 All steps completed successfully.\n"
